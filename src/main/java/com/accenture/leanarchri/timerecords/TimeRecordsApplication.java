3eaafc3bd3a89ca2390e849d2a0a18f54b030d4b
jayakrishnan <j.venugopalan@accenture.com>
2017-03-27 19:42:32 +0530

    removed unused imports

--- src/main/java/com/accenture/leanarchri/timerecords/TimeRecordsApplication.java
+++ src/main/java/com/accenture/leanarchri/timerecords/TimeRecordsApplication.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
